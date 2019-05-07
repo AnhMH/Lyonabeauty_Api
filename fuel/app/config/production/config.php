3380423be09bb3740593ab4fe32ca2908cfae8b2
AnhMH <anhmh@lifull-tech.vn>
2019-05-08 06:59:57 +0700

    Modify api config

--- fuel/app/config/production/config.php
+++ fuel/app/config/production/config.php
@@ -1,6 +1,6 @@
 <?php
 $envConf = array(
-    'img_url' => 'https://img.conlatatca.info/',
+    'img_url' => 'https://img.lyonabeauty.com/',
     'send_email' => true,
     'test_email' => '', // will send to this email for testing 
     'api_check_security' => true,
