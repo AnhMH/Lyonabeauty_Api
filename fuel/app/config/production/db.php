515ce4b509eba2bc8f8935629661cedcbaf49195
AnhMH <anhmh@lifull-tech.vn>
2019-05-07 23:35:09 +0700

    Add production config

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -6,9 +6,9 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=hoangan1_cltc',
-			'username'   => 'hoangan1_cltc',
-			'password'   => '1hoanganh',
+			'dsn'        => 'mysql:host=localhost;dbname=lyonabea_lyona',
+			'username'   => 'lyonabea_lyona',
+			'password'   => '1@Hoanganh',
 		),
 	),
 );
